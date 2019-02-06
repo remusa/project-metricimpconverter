66e1f6fb0c76f206d65f9ba75b3bbea00b3e3522
RMS <rene.as.himself@gmail.com>
2019-02-06 12:46:18 -0600

    Updated init scripts

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const runner = require('./test-runner')
 
 const app = express()
 
-app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/public', express.static(`${process.cwd()}/public`))
 
 app.use(cors({ origin: '*' })) //For FCC testing purposes only
 
@@ -41,7 +41,7 @@ app.use(helmet.noSniff())
 
 //Index page (static HTML)
 app.route('/').get(function(req, res) {
-    res.sendFile(process.cwd() + '/views/index.html')
+    res.sendFile(`${process.cwd()}/views/index.html`)
 })
 
 //For FCC testing purposes
@@ -59,19 +59,19 @@ app.use(function(req, res, next) {
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on port ' + process.env.PORT)
+    console.log(`Listening on port ${process.env.PORT}`)
 
     if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...')
-    setTimeout(function() {
-        try {
-            runner.run()
-        } catch (e) {
-            const error = e
-            console.log('Tests are not valid:')
-            console.log(error)
-        }
-    }, 1500)
+        console.log('Running Tests...')
+        setTimeout(function() {
+            try {
+                runner.run()
+            } catch (e) {
+                const error = e
+                console.log('Tests are not valid:')
+                console.log(error)
+            }
+        }, 1500)
     }
 })
 
